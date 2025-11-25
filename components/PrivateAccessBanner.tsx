bffb8f85a810a0ff6156ad35b4791d7f59d65583
ChainScriptor <cryptictiger39@gmail.com>
2025-11-25 14:48:31 +0200

    initial commit

--- components/PrivateAccessBanner.tsx
+++ components/PrivateAccessBanner.tsx
@@ -6,7 +6,7 @@ export default function PrivateAccessBanner() {
   return (
     <div className="fixed top-0 left-0 w-full bg-red-600/90 backdrop-blur-md text-white py-3 px-4 text-center text-sm font-medium z-50">
       <p className="text-white">
-        We&apos;re currently in Private Access.{" "}
+        We are currently in Private Access.{" "}
         <a
           href="#"
           className="underline decoration-dotted underline-offset-2 hover:text-red-100 transition-colors"
